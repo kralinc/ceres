dcd10a0a9df01232cbde3f8924443c4da2e307c3
kralinc <lelinxi@protonmail.com>
2023-02-14 15:59:18 -0600

    Added vuetify

--- src/main.js
+++ src/main.js
@@ -2,7 +2,17 @@ import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
+// Vuetify
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+
+const vuetify = createVuetify({
+    components,
+    directives,
+});
 
 const pinia = createPinia();
 
-createApp(App).use(router).use(pinia).mount("#app");
+createApp(App).use(router).use(pinia).use(vuetify).mount("#app");
