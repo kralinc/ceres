74994a55fff09289c6eea4e4f507c9e7b737709b
kralinc <lelinxi@protonmail.com>
2023-02-14 18:37:22 -0600

    Added rudimentary navbar

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,50 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <v-app>
+    <v-layout>
+      <v-app-bar color="primary" prominent>
+        <v-app-bar-nav-icon
+          @click="drawer = !drawer"
+          class="d-flex d-sm-none"
+        ></v-app-bar-nav-icon>
+        <v-toolbar-title>MyKitchen</v-toolbar-title>
+
+        <v-spacer></v-spacer>
+        <router-link
+          v-for="route of routes"
+          :key="route.to"
+          class="d-none d-sm-flex mr-3 router-link"
+          :to="route.to"
+        >
+          <v-btn variant="tonal">
+            {{ route.name }}
+          </v-btn>
+        </router-link>
+      </v-app-bar>
+      <v-navigation-drawer v-model="drawer" absolute temporary>
+        <router-link v-for="route of routes" :key="route.to" :to="route.to">{{
+          route.name
+        }}</router-link>
+      </v-navigation-drawer>
+    </v-layout>
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      drawer: false,
+      routes: [
+        { to: "/", name: "Home" },
+        { to: "/about", name: "About" },
+      ],
+    };
+  },
+};
+</script>
 
 <style>
 #app {
@@ -26,5 +66,10 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
+}
+
+.router-link {
+  text-decoration: none;
+  color: white;
 }
 </style>
