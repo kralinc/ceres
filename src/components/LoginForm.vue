320d25781cfa388e044225c89e6af4c9d14eb162
Isaac Kirk <isaac.kirk9@gmail.com>
2023-02-21 18:18:01 -0600

    Added sign up and reset form. Bug password inputs.

--- src/components/LoginForm.vue
+++ src/components/LoginForm.vue
@@ -1,21 +1,94 @@
 <template>
   <v-card class="pa-5">
     <v-card-title class="text-h6 text-md-h5 text-lg-h4">Sign In</v-card-title>
-    <form class="my-8" @submit.prevent="submit">
+    <v-form class="my-8" @submit.prevent="submit">
       <v-text-field class="mx-3 my-1" label="Username" required></v-text-field>
 
       <v-text-field
         v-model="password"
-        :append-inner-icon="show1 ? 'mdi-eye' : 'mdi-eye-off'"
-        :type="show1 ? 'text' : 'password'"
+        :append-inner-icon="showPassLogin ? 'mdi-eye-off' : 'mdi-eye'"
+        :type="showPassLogin ? 'password' : 'text'"
         name="input-10-1"
         label="Password"
-        @click:append-inner="show1 = !show1"
+        @click:append-inner="showPassLogin = !showPassLogin"
         class="mx-3 my-1"
       ></v-text-field>
 
-      <v-btn block class="mx-3 mt-3" @click="submitLogin"> Login </v-btn>
-    </form>
+      <v-btn block class="mx-auto mt-3" @click="submitLogin">Login</v-btn>
+    </v-form>
+  </v-card>
+
+  <v-card class="pa-5">
+    <v-card-title class="text-h6 text-md-h5 text-lg-h4">Sign Up</v-card-title>
+    <v-form @submit.prevent="submit">
+      <v-container>
+        <v-row>
+          <v-col
+            ><v-text-field
+              class="mx-3 my-1"
+              label="First Name"
+              required
+            ></v-text-field
+          ></v-col>
+          <v-col
+            ><v-text-field
+              class="mx-3 my-1"
+              label="Last Name"
+              required
+            ></v-text-field
+          ></v-col>
+        </v-row>
+        <v-text-field
+          class="mx-3 my-1"
+          label="Username"
+          required
+        ></v-text-field>
+
+        <v-text-field class="mx-3 my-1" label="Email" required></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          :append-inner-icon="showPassSignUp ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="showPassSignUp ? 'password' : 'text'"
+          name="input-10-1"
+          label="Create Password"
+          @click:append-inner="showPassSignUp = !showPassSignUp"
+          class="mx-3 my-1"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          :append-inner-icon="showPassSignUp ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="showPassSignUp ? 'password' : 'text'"
+          name="input-10-1"
+          label="Reenter Password"
+          @click:append-inner="showPassSignUp = !showPassSignUp"
+          class="mx-3 my-1"
+        ></v-text-field>
+
+        <v-btn block class="mx-auto mt-3" @click="submitLogin">Sign Up</v-btn>
+        <div class="my-3">
+          <a class="font-italic" href="url">Forgot your password?</a>
+        </div>
+      </v-container>
+    </v-form>
+  </v-card>
+
+  <v-card class="pa-5">
+    <v-card-title class="text-h6 text-md-h5 text-lg-h4"
+      >Account's Email Address</v-card-title
+    >
+    <v-form class="my-8" @submit.prevent="submit">
+      <v-text-field
+        class="mx-3 my-1"
+        label="Email Address or Username"
+        required
+      ></v-text-field>
+
+      <v-btn block class="mx-auto mt-3" @click="submitLogin"
+        >Reset Password</v-btn
+      >
+    </v-form>
   </v-card>
 </template>
 
@@ -23,7 +96,9 @@
 export default {
   data() {
     return {
-      show1: false,
+      showPassLogin: true,
+      showPassSignUp: true,
+      showPassReset: true,
     };
   },
 };
