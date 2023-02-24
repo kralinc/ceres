a66b6674472b97edb460f0072e3de691731f527b
kralinc <lelinxi@protonmail.com>
2023-02-24 12:13:54 -0600

    Started recipe view

---
+++ src/views/RecipeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Recipe #{{ $route.params.id }}</div>
+</template>
+
+<script>
+export default {
+  name: "RecipeView",
+  data() {
+    return {
+      recipeId: -1,
+    };
+  },
+};
+</script>
