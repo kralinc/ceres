b309018da54982ff386ada27585a7be701a59496
kralinc <lelinxi@protonmail.com>
2023-03-24 17:00:18 -0500

    Refactored post and getreq logic to allow for text response.

--- src/util/util.js
+++ src/util/util.js
@@ -13,18 +13,10 @@ export function buildUrl(url) {
   return TLD + url;
 }
 
-// = Parameters =
-// url: The url that you're making a get request from
-// snackbarMethod: the component method which will be used to display the snackbar message at the bottom of the page.
-// This is itself a wrapper method for a pinia action (setSnackbar) which can be found in src/stores/MainStore.js
-// ==
-// returns: json
-//
 // This method is a wrapper for the fetch API for basic get requests.
 export async function getReq(url, customText = {}) {
-  const mainStore = useMainStore();
   const token = localStorage.getItem("token");
-  return await fetch(buildUrl(url), {
+  const requestBody = {
     method: "GET", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -36,38 +28,15 @@ export async function getReq(url, customText = {}) {
     },
     redirect: "follow", // manual, *follow, error
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-  })
-    .then((response) => {
-      if (response.ok) {
-        if (customText[response.status]) {
-          mainStore.setSnackbar(customText[response.status], "green");
-        }
-        return response.json();
-      } else {
-        let errorText =
-          response.status === 403
-            ? "You're not authorized to perform this action"
-            : "Something went wrong!";
-        if (customText[response.status]) {
-          errorText = customText[response.status];
-        } else if (customText["err"]) {
-          errorText = customText["err"];
-        }
-        mainStore.setSnackbar(errorText);
-        throw new Error(errorText);
-      }
-    })
-    .catch((error) => {
-      console.error(error);
-    });
+  };
+
+  return await fetchWrapper(url, requestBody, customText);
 }
 
 // Example POST method implementation:
 export async function postReq(url = "", data = {}, customText = {}) {
-  const mainStore = useMainStore();
   const token = localStorage.getItem("token");
-  // Default options are marked with *
-  const response = await fetch(buildUrl(url), {
+  const requestBody = {
     method: "POST", // *GET, POST, PUT, DELETE, etc.
     mode: "cors", // no-cors, *cors, same-origin
     cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
@@ -80,13 +49,24 @@ export async function postReq(url = "", data = {}, customText = {}) {
     redirect: "follow", // manual, *follow, error
     referrerPolicy: "no-referrer", // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
     body: JSON.stringify(data), // body data type must match "Content-Type" header
-  })
+  };
+  return await fetchWrapper(url, requestBody, customText);
+}
+
+async function fetchWrapper(url, requestBody, customText) {
+  const mainStore = useMainStore();
+
+  return await fetch(buildUrl(url), requestBody)
     .then((response) => {
       if (response.ok) {
         if (customText[response.status]) {
           mainStore.setSnackbar(customText[response.status], "green");
         }
-        return response.json();
+        const contentType = response.headers.get("content-type");
+
+        return contentType && contentType.indexOf("application/json") !== -1
+          ? response.json()
+          : response.text();
       } else {
         let errorText =
           response.status === 403
@@ -104,5 +84,4 @@ export async function postReq(url = "", data = {}, customText = {}) {
     .catch((error) => {
       console.error(error);
     });
-  return response; // parses JSON response into native JavaScript objects
 }
