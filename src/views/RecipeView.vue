07a75068520852aaa18a9a24c627fb3f62c16653
Ashton Barnwell <KAZZHOLIO@gmail.com>
2023-03-25 19:10:23 -0500

    recipe review - big commit added ablitity to write reviews and sort reviews user wants to see

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -22,15 +22,6 @@
         </div>
         <div class="mt-5">
           <v-row>
-            <v-col sm="12" lg="6">
-              <v-text> Cook Time: {{ recipe.cookTime }} </v-text>
-            </v-col>
-            <v-col sm="12" lg="6">
-              <v-btn> Rate this Recipe ({{ reviewCount }}) </v-btn>
-            </v-col>
-          </v-row>
-          <v-divider class="mt-5"></v-divider>
-          <v-row>
             <v-col>
               <v-rating
                 readonly
@@ -41,6 +32,7 @@
               ></v-rating>
             </v-col>
           </v-row>
+          <v-divider class="mb-3"></v-divider>
         </div>
       </v-card>
     </v-col>
@@ -75,12 +67,15 @@
     <v-col lg="12"> Reviews </v-col>
   </v-row>
   <v-row>
-    <v-col lg="5">
+    <v-col lg="4">
       <v-card>
-        <v-row>
-          <v-col lg="12" class="mt-3">
-            <strong>{{ recipe.name }} Reviews </strong></v-col
+        <!-- <v-row class="mt-3">
+          <v-col sm="12" lg="6" class="mt-3">
+            <strong> Customer Reviews </strong></v-col
           >
+          <v-col sm="12" lg="6" class="mb-3">
+            <v-btn> Rate this Recipe ({{ reviewCount }}) </v-btn>
+          </v-col>
           <v-divider></v-divider>
           <v-col
             ><v-rating
@@ -91,18 +86,172 @@
               active-color="yellow-accent-4"
             ></v-rating
           ></v-col>
-        </v-row>
+        </v-row> -->
+        <v-card-title>
+          <div>
+            <v-row no-gutters>
+              <v-col lg="12" class="text-left">
+                <strong>Customer Reviews</strong>
+              </v-col>
+            </v-row>
+            <v-row no-gutters>
+              <v-col class="text-left ml-1">
+                <v-rating
+                  readonly
+                  v-model="recipe.rating"
+                  color="black"
+                  active-color="yellow-accent-4"
+                  size="18"
+                ></v-rating>
+                <span class="text-grey-lighten-2 text-caption me-2">
+                  ({{ recipe.rating }})
+                </span>
+              </v-col>
+            </v-row>
+          </div>
+        </v-card-title>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-row no-gutters>
+            <v-col cols="12" class="ma-0 pa-0">
+              <v-checkbox
+                v-model="renderReviews"
+                label="5 Stars"
+                density="compact"
+                value="5"
+                hide-details
+              ></v-checkbox>
+            </v-col>
+            <v-col cols="12" class="ma-0 pa-0">
+              <v-checkbox
+                v-model="renderReviews"
+                label="4 Stars"
+                density="compact"
+                value="4"
+                hide-details
+              ></v-checkbox>
+            </v-col>
+            <v-col cols="12" class="ma-0 pa-0">
+              <v-checkbox
+                v-model="renderReviews"
+                label="3 Stars"
+                density="compact"
+                value="3"
+                hide-details
+              ></v-checkbox>
+            </v-col>
+            <v-col cols="12" class="ma-0 pa-0">
+              <v-checkbox
+                v-model="renderReviews"
+                label="2 Stars"
+                density="compact"
+                value="2"
+                hide-details
+              ></v-checkbox>
+            </v-col>
+            <v-col cols="12" class="ma-0 pa-0">
+              <v-checkbox
+                v-model="renderReviews"
+                label="1 Stars"
+                density="compact"
+                value="1"
+                hide-details
+              ></v-checkbox>
+            </v-col>
+          </v-row>
+        </v-card-actions>
+        <v-divider></v-divider>
+        <v-card-actions>
+          <v-row>
+            <v-col>
+              <v-dialog v-model="dialog" persistent width="1024">
+                <template v-slot:activator="{ props }">
+                  <v-btn color="primary" v-bind="props" variant="outlined">
+                    Rate This Recipe
+                  </v-btn>
+                </template>
+                <v-card>
+                  <v-card-title>
+                    <span class="text-h5">Recipe Review</span>
+                  </v-card-title>
+                  <v-card-text>
+                    <v-container>
+                      <v-row>
+                        <v-col cols="12" sm="6">
+                          <v-select
+                            :items="[1, 2, 3, 4, 5]"
+                            label="Rating*"
+                            v-model="newReview.stars"
+                            required
+                          ></v-select>
+                        </v-col>
+                        <v-col cols="12" sm="6" md="4">
+                          <v-text-field
+                            label="Description"
+                            v-model="newReview.review"
+                            required
+                          ></v-text-field>
+                        </v-col>
+                      </v-row>
+                    </v-container>
+                    <small>*indicates required field</small>
+                  </v-card-text>
+                  <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn
+                      color="blue-darken-1"
+                      variant="text"
+                      @click="dialog = false"
+                    >
+                      Close
+                    </v-btn>
+                    <v-btn
+                      color="blue-darken-1"
+                      variant="text"
+                      @click="uploadReview()"
+                    >
+                      Save
+                    </v-btn>
+                  </v-card-actions>
+                </v-card>
+              </v-dialog>
+            </v-col>
+          </v-row>
+        </v-card-actions>
       </v-card>
     </v-col>
-    <v-col lg="7" v-for="review in reviews" :key="review.id">
-      <v-card>
-        <div>
-          {{ review.user.username }}
-          <v-card>
-            {{ review.review }}
-          </v-card>
-        </div>
-      </v-card>
+    <v-col lg="8">
+      <div v-for="review in reviews" :key="review.id" class="mb-5">
+        <v-card v-if="review.show">
+          <div class="ma-1">
+            <div class="mx-3">
+              <v-card-actions>
+                <a>{{ review.user.username }}</a>
+
+                <v-spacer></v-spacer>
+
+                <span class="text-grey-lighten-2 text-caption me-2">
+                  ({{ review.stars }})
+                </span>
+
+                <v-rating
+                  readonly
+                  v-model="review.stars"
+                  color="black"
+                  active-color="yellow-accent-4"
+                  size="25"
+                ></v-rating>
+              </v-card-actions>
+            </div>
+            <v-divider class="ma-3"></v-divider>
+            <v-card-text class="text-left">
+              {{ review.review }}
+            </v-card-text>
+          </div>
+        </v-card>
+      </div>
     </v-col>
   </v-row>
 </template>
@@ -110,14 +259,14 @@
 <script>
 import { mapStores } from "pinia";
 import { useMainStore } from "@/stores/MainStore";
-import { getReq } from "@/util/util.js";
+import { getReq, postReq } from "@/util/util.js";
 
 export default {
   name: "RecipeView",
   mounted() {
     this.loadRecipe(this.$route.params.id);
     this.loadRecipeItems(this.$route.params.id);
-    // this.loadReviews(this.$route.params.id);
+    this.loadReviews(this.$route.params.id);
   },
   data() {
     return {
@@ -125,7 +274,27 @@ export default {
       recipe: Object,
       itemsList: [],
       reviews: [],
+      renderReviews: [],
+      dialog: false,
+      newReview: {},
     };
+  },
+  watch: {
+    renderReviews() {
+      if (this.renderReviews.length > 0) {
+        this.reviews.forEach((review) => {
+          if (this.renderReviews.includes(review.stars.toString())) {
+            review.show = true;
+          } else {
+            review.show = false;
+          }
+        });
+      } else {
+        this.reviews.forEach((review) => {
+          review.show = true;
+        });
+      }
+    },
   },
   computed: {
     ...mapStores(useMainStore),
@@ -152,6 +321,24 @@ export default {
       this.reviews = await getReq("v1/api/recipes/getRecipeReviews?id=" + id, {
         err: "There was a problem loading reviews!",
       });
+      this.reviews.forEach((review) => {
+        review.show = true;
+      });
+    },
+    async uploadReview() {
+      const uploadReview = {
+        stars: this.newReview.stars,
+        review: this.newReview.review,
+        recipeId: this.$route.params.id,
+      };
+      const pantry = await postReq("v1/api/recipes/addReview", uploadReview, {
+        200: "Review Successfully Added!",
+        err: "Could not update reviews.",
+        403: "You need to be logged in to add a review.",
+      });
+      if (pantry) {
+        console.log(pantry);
+      }
     },
 
     // showLoadRecipeError() {
