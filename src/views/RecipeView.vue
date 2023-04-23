f63e179eeae54682f7c1b78cc1ce586c2412734f
Ashton Barnwell <KAZZHOLIO@gmail.com>
2023-04-23 17:57:32 -0500

    complete recipe - basically final changes got little bugs to fix first

--- src/views/RecipeView.vue
+++ src/views/RecipeView.vue
@@ -1,6 +1,5 @@
 <template>
   <v-row>
-    {{ itemsList }}
     <v-col cols="12">
       <v-card>
         <h1 class="text-h1 py-5">{{ recipe.name }}</h1>
@@ -14,7 +13,7 @@
           <v-img
             cover
             :src="
-              recipe.image ? recipe.image : 'https://placekitten.com/200/200'
+              recipe.picId ? recipe.picId : 'https://placekitten.com/200/200'
             "
           >
           </v-img>
@@ -25,7 +24,7 @@
               <v-rating
                 readonly
                 size="x-large"
-                v-model="recipe.rating"
+                v-model="recipe.avgRating"
                 color="black"
                 active-color="yellow-accent-4"
               ></v-rating>
@@ -37,7 +36,17 @@
     </v-col>
     <v-col sm="12" lg="7">
       <v-row>
-        <v-col>{{ recipe.description }}</v-col>
+        <v-col>
+          <ol>
+            <li
+              v-for="descriptor in recipe.description"
+              :key="descriptor"
+              class="ml-4 pa-2 text-left text-h5"
+            >
+              {{ descriptor }}
+            </li>
+          </ol>
+        </v-col>
         <v-divider></v-divider>
       </v-row>
       <v-row>
@@ -53,7 +62,10 @@
             <tbody>
               <tr v-for="item in itemsList" :key="item.id">
                 <td>{{ item.foodItem.name }}</td>
-                <td>{{ item.quantity }} {{ item.measurementUnit }}</td>
+                <td>
+                  {{ parseFloat(item.quantity.toFixed(3)) }}
+                  {{ item.measurementUnit }}
+                </td>
               </tr>
             </tbody>
           </v-table>
@@ -61,76 +73,104 @@
       </v-row>
     </v-col>
   </v-row>
-  <v-divider class="ma-5"></v-divider>
-  <v-row>
-    <v-col>
-      <v-dialog v-model="dialog[0]" persistent width="1024">
-        <template v-slot:activator="{ props }">
-          <v-btn
-            color="primary"
-            v-bind="props"
-            variant="outlined"
-            @click="loadCompleteList()"
-          >
-            Complete Recipe
-          </v-btn>
-        </template>
-        <v-card>
-          <v-card-title>
-            <span class="text-h5">Ingredients</span>
-          </v-card-title>
-          <v-card-text>
-            <v-container>
-              <v-table>
-                <thead>
-                  <tr>
-                    <th class="text-left">Ingredient</th>
-                    <th class="text-left">Amount in Pantry</th>
-                    <th class="text-left">Amount Used</th>
-                    <th class="text-left">Amount Left</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  <tr v-for="item in completeList" :key="item.id">
-                    <td>{{ item.foodItem.name }}</td>
-                    <td>{{ item.pantryCount }}</td>
-                    <td>
-                      <v-text-field
-                        v-model="item.quantity"
-                        hide-details
-                        single-line
-                        type="number"
-                        clearable
-                      ></v-text-field>
-                    </td>
-                    <td>{{ item.pantryCount - item.quantity }}</td>
-                  </tr>
-                </tbody>
-              </v-table>
-            </v-container>
-            <small>*indicates required field</small>
-          </v-card-text>
-          <v-card-actions>
-            <v-spacer></v-spacer>
+  <template v-if="token">
+    <v-divider class="ma-5"></v-divider>
+    <v-row>
+      <v-col>
+        <v-dialog v-model="dialog[0]" persistent width="1024">
+          <template v-slot:activator="{ props }">
             <v-btn
-              color="blue-darken-1"
-              variant="text"
-              @click="dialog[0] = false"
-            >
-              Close
-            </v-btn>
-            <v-btn
-              color="blue-darken-1"
-              variant="text"
-              @click="completeRecipe()"
+              color="primary"
+              v-bind="props"
+              variant="outlined"
+              @click="loadCompleteList()"
             >
               Complete Recipe
             </v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-dialog>
-    </v-col>
-  </v-row>
+          </template>
+          <v-card>
+            <v-card-title>
+              <span class="text-h5">Ingredients</span>
+            </v-card-title>
+            <v-card-text>
+              <v-container>
+                <v-table>
+                  <thead>
+                    <tr>
+                      <th class="text-left">Ingredient</th>
+                      <th class="text-left">Amount in Pantry</th>
+                      <th class="text-left">Amount Used</th>
+                      <th class="text-left">Unit</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr v-for="item in completeList" :key="item.id">
+                      <td>{{ item.foodItem.name }}</td>
+                      <template v-if="item.pantryCount">
+                        <td>
+                          {{ parseFloat(item.pantryCount.toFixed(3)) }}
+                          {{ item.pantryUnit }}
+                        </td>
+                      </template>
+                      <template v-else> <td>NONE IN PANTRY</td> </template>
+                      <td>
+                        <v-text-field
+                          v-model="item.quantityRounded"
+                          hide-details
+                          single-line
+                          type="number"
+                          clearable
+                        ></v-text-field>
+                      </td>
+                      <template v-if="unitSystem.metric">
+                        <td>
+                          <v-select
+                            v-model="item.measurementUnit"
+                            :items="metricUnits"
+                            single-line
+                            hide-details
+                          ></v-select>
+                        </td>
+                      </template>
+                      <template v-else>
+                        <td>
+                          <v-select
+                            v-model="item.measurementUnit"
+                            :items="imperialUnits"
+                            single-line
+                            hide-details
+                          ></v-select>
+                        </td>
+                      </template>
+                    </tr>
+                  </tbody>
+                </v-table>
+              </v-container>
+              <small>*indicates required field</small>
+            </v-card-text>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                @click="dialog[0] = false"
+              >
+                Close
+              </v-btn>
+              <v-btn
+                color="blue-darken-1"
+                variant="text"
+                :disabled="completeFlag"
+                @click="completeRecipe()"
+              >
+                Complete Recipe
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-col>
+    </v-row>
+  </template>
   <v-divider class="ma-5"></v-divider>
   <v-row>
     <v-col lg="12"> Reviews </v-col>
@@ -149,13 +189,13 @@
               <v-col class="text-left ml-1">
                 <v-rating
                   readonly
-                  v-model="recipe.rating"
+                  v-model="recipe.avgRating"
                   color="black"
                   active-color="yellow-accent-4"
-                  size="18"
+                  size="medium"
                 ></v-rating>
                 <span class="text-grey-lighten-2 text-caption me-2">
-                  ({{ recipe.rating }})
+                  ({{ recipe.avgRating }})
                 </span>
               </v-col>
             </v-row>
@@ -318,14 +358,19 @@ export default {
     this.loadRecipe(this.$route.params.id);
     this.loadRecipeItems(this.$route.params.id);
     this.loadReviews(this.$route.params.id);
-    this.pantryItems = await postReq("v1/api/inventory/getInventory", {});
+    if (this.token) {
+      this.pantryItems = await postReq("v1/api/inventory/getInventory", {});
+      this.getUnitSystem();
+    }
     this.visiblePantryItems = this.pantryItems;
   },
   data() {
     return {
+      token: localStorage.getItem("token"),
       recipeId: -1,
       recipe: Object,
       itemsList: [],
+      completeFlag: false,
       reviews: [],
       renderReviews: [],
       dialog: [false, false],
@@ -334,6 +379,30 @@ export default {
       pantryItems: [],
       visiblePantryItems: [],
       completeList: [],
+      unitSystem: "",
+      metricUnits: [
+        "Meter",
+        "Piece",
+        "Celsius",
+        "Milliliter",
+        "Liter",
+        "Milligram",
+        "Gram",
+        "Kilogram",
+      ],
+      imperialUnits: [
+        "Inches",
+        "Feet",
+        "Yard",
+        "Slices",
+        "Fahrenheit",
+        "Teaspoon",
+        "Tablespoon",
+        "Cup",
+        "Gallon",
+        "Ounce",
+        "Pound",
+      ],
     };
   },
   watch: {
@@ -360,10 +429,16 @@ export default {
     },
   },
   methods: {
+    async getUnitSystem() {
+      this.unitSystem = await postReq("v1/api/user/getUserPreferences", {
+        err: "There was a problem loading user preferences!",
+      });
+    },
     async loadRecipe(id) {
       this.recipe = await getReq("v1/api/recipes/getRecipe?id=" + id, {
         err: "There was a problem loading the recipe!",
       });
+      this.recipe.description = this.recipe.description.split("\n");
     },
     async loadRecipeItems(id) {
       this.itemsList = await getReq("v1/api/recipes/getRecipeItems?id=" + id, {
@@ -392,6 +467,7 @@ export default {
       if (review) {
         console.log(review);
       }
+      this.dialog[1] = false;
     },
     loadCompleteList() {
       this.completeList = this.itemsList;
@@ -399,8 +475,13 @@ export default {
         this.pantryItems.forEach((pantryItem) => {
           if (pantryItem.foodId.id == item.foodItem.id) {
             item.pantryCount = pantryItem.quantity;
+            item.pantryUnit = pantryItem.unit;
           }
         });
+        if (!item.pantryCount) {
+          this.completeFlag = true;
+        }
+        item.quantityRounded = parseFloat(item.quantity.toFixed(3));
       });
     },
     async completeRecipe() {
@@ -408,11 +489,18 @@ export default {
         this.removeItem(item);
       });
       this.pantryItems = await postReq("v1/api/inventory/getInventory", {});
+      this.dialog[0] = false;
     },
     async removeItem(item) {
+      let submitQuantity = 0;
+      if (parseInt(item.quantity.toFixed(3)) == item.quantityRounded) {
+        submitQuantity = item.quantity;
+      } else {
+        submitQuantity = item.quantityRounded;
+      }
       const updateInventory = {
         foodId: item.foodItem.id,
-        quantity: parseInt(item.quantity) * -1,
+        quantity: parseInt(submitQuantity) * -1,
         unit: item.measurementUnit,
       };
       await postReq("v1/api/inventory/updateInventory", updateInventory, {
